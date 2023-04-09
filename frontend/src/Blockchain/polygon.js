c627e9fd59d4433751d9fc996e1e00a123767d72
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-09 12:06:41 +0530

    provider config updated

--- frontend/src/Blockchain/polygon.js
+++ frontend/src/Blockchain/polygon.js
@@ -3,27 +3,40 @@ import CaptureTheFlag from "../artifacts/contracts/CaptureTheFlag.sol/CaptureThe
 
 import { RelayProvider } from "@opengsn/provider";
 
-const paymasterAddress = "0xc5d5d8f0ea82888e35fb573ba9403cde1c87c45b";
+const WhitelistPaymasterAddress = "0xc5d5d8f0ea82888e35fb573ba9403cde1c87c45b";
+const AcceptEverythingPaymaster = "0x086c11bd5A61ac480b326916656a33c474d1E4d8";
 
 const CAPTURE_THE_FLAG_CONTRACT_ADDRESS =
   "0x8bf1aa01c29b283fb5a036ce687f36c8ae7f0901";
 
-const gsnProvider = await RelayProvider.newProvider({
-  provider: window.ethereum,
-  config: {
-    paymasterAddress,
-  },
-}).init();
+// const gsnProvider = await RelayProvider.newProvider({
+//   provider: window.ethereum,
+//   config: {
+//     WhitelistPaymasterAddress,
+//   },
+// });
+// export const provider = new ethers.providers.Web3Provider(gsnProvider);
 
-export const provider = new ethers.providers.Web3Provider(gsnProvider);
-export const signer = provider.getSigner();
+const gsnConfig = {
+  paymasterAddress: WhitelistPaymasterAddress,
+};
+
+const gsnProvider = RelayProvider.newProvider({
+  provider: window.ethereum,
+  config: gsnConfig,
+});
+await gsnProvider.init();
+
+const etherProvider = new ethers.providers.Web3Provider(gsnProvider);
+
+export const signer = etherProvider.getSigner();
 
 console.log(signer._address);
 
 export const ReadContracts = new ethers.Contract(
   CAPTURE_THE_FLAG_CONTRACT_ADDRESS,
   CaptureTheFlag.abi,
-  provider
+  etherProvider
 );
 
 export const WriteContracts = new ethers.Contract(
@@ -31,3 +44,14 @@ export const WriteContracts = new ethers.Contract(
   CaptureTheFlag.abi,
   signer
 );
+
+/*
+
+Capture the Flag 
+- 0x164e7eC02DCF25CF6c8a1DfEA5fB4e7f16b6c112
+- 0x8bf1aa01c29b283fb5a036ce687f36c8ae7f0901
+
+Whitelist Paymaster
+- 
+
+*/
